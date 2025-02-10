8e91854a56f28b4c5cce3652dc06534e1e7c8e55
1yavuz <1yavuzahmet@gmail.com>
2025-02-10 19:55:10 +0300

    register validation added

--- src/components/AuthHeader.jsx
+++ src/components/AuthHeader.jsx
@@ -1,12 +1,25 @@
 import React from "react";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Button from '@mui/material/Button';
+import "../../app.css"
+
 const AuthHeader = () => {
   return (
-    <Grid item xs={12} mb={3}>
-      <Typography variant="h3" color="primary" align="center">
-        Stock Management App
-      </Typography>
+    <Grid item xs={12} mb={3} direction="row-reverse">
+      <Box sx={{ flexGrow: 1 }} >
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h3" component="div" sx={{ flexGrow: 1, textAlign: "center", fontFamily: "Playfair Display" }}>
+              Stock Sage Management
+            </Typography>
+            <Button color="inherit">Login</Button>
+          </Toolbar>
+        </AppBar>
+      </Box>
     </Grid>
   );
 };
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -2,12 +2,39 @@ import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
 import Avatar from "@mui/material/Avatar";
 import LockIcon from "@mui/icons-material/Lock";
-import image from "../assets/regi.avif";
+import image from "../assets/register.jpg";
 import Grid from "@mui/material/Grid";
 import { Link } from "react-router-dom";
-import { Box } from "@mui/material";
+import { Box, Button, TextField } from "@mui/material";
 import AuthHeader from "../components/AuthHeader";
 import AuthImage from "../components/AuthImage";
+import { Formik } from 'formik';
+import * as Yup from 'yup';
+
+const SignupSchema = Yup.object().shape({
+  username: Yup.string()
+    .min(2, 'Too Short!')
+    .max(50, 'Too Long!')
+    .required('Required'),
+  firstName: Yup.string()
+    .min(2, 'Too Short!')
+    .max(50, 'Too Long!')
+    .required('Required'),
+  lastName: Yup.string()
+    .min(2, 'Too Short!')
+    .max(50, 'Too Long!')
+    .required('Required'),
+  email: Yup.string().email('Invalid email').required('Required'),
+  password: Yup.string()
+    .min(8, 'Too Short!')
+    .max(50, 'Too Long!')
+    .matches(/[A-Z]/, 'Must contain at least one uppercase letter')
+    .matches(/[a-z]/, 'Must contain at least one lowercase letter')
+    .matches(/\d/, 'Must contain at least one number')
+    .matches(/[!@#$%^&*+=]/, 'Must contain at least one special character')
+    .matches(/^[^\s]+$/, 'Cannot contain spaces')
+    .required('Required')
+});
 
 const Register = () => {
   return (
@@ -43,13 +70,94 @@ const Register = () => {
           >
             Register
           </Typography>
-
-          <Box sx={{ textAlign: "center", mt: 2, color:"secondary.main" }}>
+          <Formik
+            initialValues={{
+              username: "",
+              firstName: "",
+              lastName: "",
+              email: "",
+              password: "",
+            }}
+            validationSchema={SignupSchema
+            }
+            onSubmit={{
+            }}
+          >
+            {({
+              values,
+              errors,
+              touched,
+              handleChange,
+              handleBlur,
+              handleSubmit,
+              isSubmitting,
+            }) => (
+              <form onSubmit={handleSubmit}>
+                <TextField
+                  name="username"
+                  label="User Name"
+                  variant="outlined"
+                  fullWidth
+                  value={values.username}
+                  onChange={handleChange}
+                  helperText={touched.username && errors.username}
+                  onBlur={handleBlur}
+                  error={touched.username && errors.username}
+                  margin="normal" />
+                <TextField
+                  name="firstName"
+                  label="First Name"
+                  variant="outlined"
+                  fullWidth
+                  value={values.firstName}
+                  onChange={handleChange}
+                  helperText={touched.firstName && errors.firstName}
+                  onBlur={handleBlur}
+                  error={touched.firstName && errors.firstName}
+                  margin="normal" />
+                <TextField
+                  name="lastName"
+                  label="Last Name"
+                  variant="outlined"
+                  fullWidth
+                  value={values.lastName}
+                  onChange={handleChange}
+                  helperText={touched.lastName && errors.lastName}
+                  onBlur={handleBlur}
+                  error={touched.lastName && errors.lastName}
+                  margin="normal" />
+                <TextField
+                  name="email"
+                  label="e-mail"
+                  variant="outlined"
+                  fullWidth
+                  value={values.email}
+                  onChange={handleChange}
+                  helperText={touched.email && errors.email}
+                  onBlur={handleBlur}
+                  error={touched.email && errors.email}
+                  margin="normal" />
+                <TextField
+                  name="password"
+                  label="Password"
+                  variant="outlined"
+                  fullWidth
+                  value={values.password}
+                  onChange={handleChange}
+                  helperText={touched.password && errors.password}
+                  onBlur={handleBlur}
+                  error={touched.password && errors.password}
+                  margin="normal" />
+                <Button variant="contained" color="success" type="submit" fullWidth>Sign Up</Button>
+              </form>
+            )}
+          </Formik>
+          <Box sx={{ textAlign: "center", mt: 2, color: "secondary.main" }}>
             <Link to="/">Already have an account? Sign in</Link>
           </Box>
         </Grid>
 
-    <AuthImage image={image} />
+        <AuthImage image={image} />
       </Grid>
     </Container>
   );
