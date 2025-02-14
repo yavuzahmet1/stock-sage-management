f1f6572105803c6f1932758fa11b320391195d2b
1yavuz <1yavuzahmet@gmail.com>
2025-02-14 22:49:44 +0300

    stockSlice created and defined in store

--- src/app/store.jsx
+++ src/app/store.jsx
@@ -2,6 +2,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage"; // localStorage için varsayılan depolama
 import authReducer from "../features/authSlice";
+import stockReducer from "../features/stockSlice";
 
 // Redux Persist yapılandırması
 const persistConfig = {
@@ -17,6 +18,7 @@ const persistedReducer = persistReducer(persistConfig, authReducer);
 const store = configureStore({
   reducer: {
     auth: persistedReducer, // Persist edilmiş reducer'ı kullan
+    stock: stockReducer
   },
   devTools: process.env.NODE_ENV !== "production", // Geliştirme ortamında Redux DevTools'u etkinleştir
   middleware: (getDefaultMiddleware) =>
