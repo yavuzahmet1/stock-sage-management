e18276ce9e5a0a14a315e358fb1f6fbd5950a748
1yavuz <1yavuzahmet@gmail.com>
2025-02-18 18:43:11 +0300

    Products page connected to product table

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,8 +1,22 @@
+import { Button, Container, Typography } from '@mui/material';
 import React from 'react'
+import ProductsTable from '../components/Table/ProductsTable';
 
 const Products = () => {
     return (
-        <div>Products</div>
+        <div>
+            <Container>
+                <Typography fontFamily="Apple Color Emoji" textAlign="center" variant='h4' >PRODUCTS</Typography>
+
+
+                <Button
+                    variant="contained"
+                >
+                    Add Product
+                </Button>
+                <ProductsTable />
+            </Container>
+        </div>
     )
 }
 
