3402fa4f65f6af05edd3055a7a5a3d4a842e0ecb
1yavuz <1yavuzahmet@gmail.com>
2025-02-18 23:55:39 +0300

    productModal completed

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,24 +1,38 @@
 import { Button, Container, Typography } from '@mui/material';
 import ProductsTable from '../components/Table/ProductsTable';
 import useStockCall from '../hook/useStockCall'
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import ProductModal from '../components/Modal/ProductModal';
 
 const Products = () => {
     const { getStockData } = useStockCall()
+
+    const { selectedData, setSelectedData } = useState({
+        category: "",
+        brandId: "",
+        name: ""
+    })
+
+    const [open, setOpen] = useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
 
     useEffect(() => {
         getStockData("products")
         getStockData("brands")
         getStockData("categories")
     })
+
+
     return (
         <div>
             <Container>
                 <Typography fontFamily="Apple Color Emoji" textAlign="center" variant='h4' >PRODUCTS</Typography>
-
+                <ProductModal open={open} handleClose={handleClose} selectedData={selectedData} />
 
                 <Button
                     variant="contained"
+                    onClick={handleOpen}
                 >
                     Add Product
                 </Button>
