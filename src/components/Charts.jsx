1944e147dbd0e5ec1d2e26c07c52243c1712f7ad
1yavuz <1yavuzahmet@gmail.com>
2025-02-23 22:30:05 +0300

    chart flex added

--- src/components/Charts.jsx
+++ src/components/Charts.jsx
@@ -20,61 +20,57 @@ const Charts = () => {
     }))
 
     return (
-        <Card>
-            <Grid2 container spacing={2} mt={3}>
-                <Grid2 item="true" >
-                    <CardContent xs={12} md={6}
-                        sx={{
-                            boxShadow: 3,
-                            borderRadius: 2,
-                            margin: 2,
-                            padding: 3,
-                        }}
-                    >
-                        <Typography variant="h6">Sales</Typography>
-                        <AreaChart width={675} height={300} data={sales}>
-                            <CartesianGrid strokeDasharray="3 3" />
-                            <XAxis dataKey="createdAt" />
-                            <YAxis dataKey={salesDate.amount} />
-                            <Tooltip />
-                            <Legend />
-                            <Area
-                                type="monotone"
-                                dataKey="amount"
-                                stroke="#8884d8"
-                                fill="#8884d8"
-                                fillOpacity={0.1}
-                            />
-                        </AreaChart>
-                    </CardContent>
-                </Grid2>
-                <Grid2 item="true" >
-                    <CardContent xs={12} md={6}
-                        sx={{
-                            boxShadow: 3,
-                            borderRadius: 2,
-                            margin: 2,
-                            padding: 3,
-                        }}
-                    >
-                        <Typography variant="h6">Purchases</Typography>
-                        <AreaChart width={675} height={300} data={purchases}>
-                            <CartesianGrid strokeDasharray="3 3" />
-                            <XAxis dataKey="createdAt" />
-                            <YAxis />
-                            <Tooltip />
-                            <Legend />
-                            <Area
-                                type="monotone"
-                                dataKey="amount"
-                                stroke="#82ca9d"
-                                fill="#82ca9d"
-                                fillOpacity={0.1}
-                            />
-                        </AreaChart>
-                    </CardContent>
-                </Grid2>
-            </Grid2>
+        <Card sx={{ display: "flex" }}>
+
+            <CardContent xs={12} md={6}
+                sx={{
+                    boxShadow: 3,
+                    borderRadius: 2,
+                    margin: 2,
+                    padding: 3,
+                }}
+            >
+                <Typography variant="h6">Sales</Typography>
+                <AreaChart width={675} height={300} data={sales}>
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="createdAt" />
+                    <YAxis dataKey={salesDate.amount} />
+                    <Tooltip />
+                    <Legend />
+                    <Area
+                        type="monotone"
+                        dataKey="amount"
+                        stroke="#8884d8"
+                        fill="#8884d8"
+                        fillOpacity={0.1}
+                    />
+                </AreaChart>
+            </CardContent>
+            <CardContent xs={12} md={6}
+                sx={{
+                    boxShadow: 3,
+                    borderRadius: 2,
+                    margin: 2,
+                    padding: 3,
+                }}
+            >
+                <Typography variant="h6">Purchases</Typography>
+                <AreaChart width={675} height={300} data={purchases}>
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="createdAt" />
+                    <YAxis />
+                    <Tooltip />
+                    <Legend />
+                    <Area
+                        type="monotone"
+                        dataKey="amount"
+                        stroke="#82ca9d"
+                        fill="#82ca9d"
+                        fillOpacity={0.1}
+                    />
+                </AreaChart>
+            </CardContent>
+
         </Card >
     )
 }
